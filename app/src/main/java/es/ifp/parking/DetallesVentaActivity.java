9b141f50d0f3e8f65fc23cb9cc275585a80c39d5
NU <ainhoa.arar@gmail.com>
2023-12-14 18:34:07 +0100

    Nuevas clases

--- app/src/main/java/es/ifp/parking/DetallesVentaActivity.java
+++ app/src/main/java/es/ifp/parking/DetallesVentaActivity.java
@@ -3,8 +3,21 @@ package es.ifp.parking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import java.time.DateTimeException;
+import java.util.Date;
 
 public class DetallesVentaActivity extends AppCompatActivity {
+
+    protected EditText fecha;
+    protected EditText hora;
+    protected EditText latitud;
+    protected EditText longitud;
+
+    private Date contenidoFecha;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
